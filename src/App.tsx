74ada14daa3c27973dc43ebedb24702c01e6c6ef
Peter <hoffmannpeter82@gmail.com>
2023-10-13 12:46:05 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardList,
+  Collapse,
+  Menu,
+  MenuItem,
+  Popover,
+} from "@blueprintjs/core";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const accountMenu = (
+    <Menu>
+      <MenuItem icon="log-in" text="Login" />
+      <MenuItem icon="log-out" text="Logout" />
+    </Menu>
   );
-}
+
+  return (
+    <>
+      <Button>I am a blueprintjs button</Button>
+      <div>
+        <ButtonGroup>
+          <Button icon="play">Play</Button>
+          <Button icon="stop">Stop</Button>
+          <Button icon="fast-forward">Forward</Button>
+          <Button icon="fast-backward">Backward</Button>
+        </ButtonGroup>
+      </div>
+
+      <div>
+        <Button onClick={() => setOpen((previous) => !previous)}>
+          Open collapsible
+        </Button>
+
+        <Collapse isOpen={open}>
+          <CardList bordered={true}>
+            <Card>First</Card>
+            <Card>Second</Card>
+            <Card>Third</Card>
+          </CardList>
+        </Collapse>
+      </div>
+
+      <div>
+        <Popover content={accountMenu} isOpen={openMenu}>
+          <Button
+            alignText="left"
+            fill={true}
+            icon="applications"
+            rightIcon="caret-down"
+            text="Account"
+            onClick={() => setOpenMenu((previous) => !previous)}
+          />
+        </Popover>
+      </div>
+    </>
+  );
+};
 
 export default App;
